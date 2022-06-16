9f5175c988b9de42a85f47706145955a6f2af548
hakanscotland <hakansukrudag@gmail.com>
2022-06-16 14:12:25 +0100

    jenkins file Update

--- tests/Automationtest.spec.js
+++ tests/Automationtest.spec.js
@@ -1,29 +1,6 @@
 // @ts-check
 const {test, expect} = require('@playwright/test');
 
-
-test.skip('@Web UI Controls', async ({page})=>
- {
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    // @ts-ignore
-    const userName = page.locator('#username');
-    // @ts-ignore
-    const signIn = page.locator("#signInBtn");
-
-    const documentLink = page.locator("[href*='documents-request']");
-    const dropdown = page.locator("select.form-control");
-    await dropdown.selectOption("consult");
-    await page.locator(".radiotextsty").last().click();
-    await page.locator("#okayBtn").click();
-    console.log(await page.locator(".radiotextsty").last().isChecked());
-    await expect(page.locator(".radiotextsty").last()).toBeChecked();
-    await page.locator("#terms").click();
-    await expect( page.locator("#terms")).toBeChecked();
-    await page.locator("#terms").uncheck();
-    expect( await page.locator("#terms").isChecked()).toBeFalsy();
-    await expect(documentLink).toHaveAttribute("class","blinkingText");
- });
-
  
  test.only('GlobalTest', async ({page})=>
  
